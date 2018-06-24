ae6ac84f40374fac3dbee7f77a578dfba46eee8d
LX1993728 <2652790899@qq.com>
2018-06-24 08:23:18 +0800

    spring boot admin -part1

---
+++ 2.spring-boot-admin/admin1/admin-server/src/main/java/com/liuxun/cloud/AdminServerApplication.java
@@ -0,0 +1,15 @@
+package com.liuxun.cloud;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableAdminServer
+@EnableEurekaClient
+public class AdminServerApplication {
+    public static void main(String[] args){
+        SpringApplication.run(AdminServerApplication.class,args);
+    }
+}
---
+++ 2.spring-boot-admin/admin2/admin-server/src/main/java/com/liuxun/cloud/AdminServerApplication.java
@@ -0,0 +1,20 @@
+package com.liuxun.cloud;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@SpringBootApplication
+@EnableTurbine
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableAdminServer
+public class AdminServerApplication {
+    public static void main(String[] args){
+        SpringApplication.run(AdminServerApplication.class,args);
+    }
+}
