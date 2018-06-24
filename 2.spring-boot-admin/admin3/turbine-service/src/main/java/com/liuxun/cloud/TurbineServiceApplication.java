6d0670acd4af034a35695e98fe9a791d53ebafca
LX1993728 <2652790899@qq.com>
2018-06-24 12:06:43 +0800

    spring boot admin -part2

---
+++ 2.spring-boot-admin/admin3/turbine-service/src/main/java/com/liuxun/cloud/TurbineServiceApplication.java
@@ -0,0 +1,13 @@
+package com.liuxun.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@SpringBootApplication
+@EnableTurbine
+public class TurbineServiceApplication {
+    public static void main(String[] args){
+        SpringApplication.run(TurbineServiceApplication.class,args);
+    }
+}
